8bdeb43cc05eb6645461394f05a5356435a668e2
Huzaifa Zahid Shah <158118730+huzaifazshah01@users.noreply.github.com>
2026-01-20 16:52:41 +0530

    Update API base URL for production environment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api"
-  baseURL:"https://banking-system-wwxe.onrender.com/api",
+  baseURL:"https://banking-sys-s2xj.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
