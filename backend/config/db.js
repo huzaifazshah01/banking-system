fc81fbe189482604ed4d07e58630f5f8f812b4c1
Huzaifa Zahid Shah <158118730+huzaifazshah01@users.noreply.github.com>
2026-01-20 15:41:09 +0530

    Switch database connection from MySQL to PostgreSQL

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,10 +1,21 @@
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',
-  database: 'bank',
+// const db = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   password: 'root',
+//   database: 'bank',
+// });
+
+// module.exports = db
+
+const { Pool } = require("pg");
+
+const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = db
+module.exports = db;
