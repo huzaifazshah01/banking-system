78dc43a3088b0aa090cc8b14f83c34ac3846b8a5
Huzaifa Zahid Shah <158118730+huzaifazshah01@users.noreply.github.com>
2026-01-20 15:58:59 +0530

    Update API baseURL to include '/api' endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:5000/api"
-  baseURL:"https://banking-system-wwxe.onrender.com",
+  baseURL:"https://banking-system-wwxe.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
