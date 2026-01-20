47f8f4f7148954570cf63d6debba501a231a350b
Huzaifa Zahid Shah <158118730+huzaifazshah01@users.noreply.github.com>
2026-01-20 17:33:57 +0530

    Simplify CORS configuration in app.js
    Replaced specific CORS configuration with default settings.

--- backend/app.js
+++ backend/app.js
@@ -8,12 +8,7 @@ const bankerRoutes = require('./routes/bankerRoutes');
 
 const app = express();
 
-app.use(cors(
-  {
-    origin: 'http://banking-system-sigma-lime.vercel.app',
-    credentials: true
-  }
-));
+app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
