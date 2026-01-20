7ab9f046f8d03d6d9ceafbe9d2f2af580dd1d559
Huzaifa Zahid Shah <158118730+huzaifazshah01@users.noreply.github.com>
2026-01-20 17:12:22 +0530

    Configure CORS with specific origin and credentials

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,12 @@ const bankerRoutes = require('./routes/bankerRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+  {
+    origin: 'http://banking-system-sigma-lime.vercel.app',
+    credentials: true
+  }
+));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
